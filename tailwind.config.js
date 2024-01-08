a6eede74fa8a9705fa51bec9427f4cc25c89e05a
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-01-09 01:39:31 +0600

    Navbar & banner text

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#fd5442",
+				secondary: "#ebedef",
+			},
+		},
+	},
+	plugins: [],
+};
